9cbd5f01adb04670d00ebc59813e55acb9e8b44e
qiaogw <qiaogw@sub-cloud.com>
2023-10-24 10:18:13 +0800

    版本

---
+++ gen/pkgCode.go
@@ -0,0 +1,115 @@
+package gen
+
+import (
+	"fmt"
+	"github.com/qiaogw/gocode/global"
+	"github.com/qiaogw/gocode/model"
+	"github.com/qiaogw/gocode/setting"
+	utils2 "github.com/qiaogw/gocode/util"
+	"github.com/zeromicro/go-zero/tools/goctl/pkg/golang"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func Gen(genApp *AutoCodeService, db *model.Db) error {
+	apiPackage := filepath.Join("TempDown", db.Package)
+	//genApp := gen.AutoCodeServiceApp
+	db.Package = strings.ToLower(db.Package)
+	err := genApp.CreateConfig(db)
+	if err != nil {
+		return err
+	}
+	configYml := global.GetConfigFile(apiPackage)
+	// 读取配置
+	global.GenViper = setting.Viper(configYml)
+	ed, err := setting.GormInit()
+	if err != nil {
+		return err
+	}
+	global.GenDB = ed
+	return Code(genApp)
+}
+
+func Code(genApp *AutoCodeService) error {
+	genApp.Init()
+	fmt.Printf(utils2.Green(fmt.Sprintf("数据库连接成功，类型为：%s,地址为：%s:%v,数据库为：%s\n",
+		global.GenDB.Name(), global.GenConfig.DB.Path, global.GenConfig.DB.Port, global.GenConfig.DB.Dbname)))
+	tables, err := genApp.DB.GetTables(global.GenConfig.DB.Dbname)
+	if err != nil {
+		log.Println(utils2.Red(fmt.Sprintf("获取表 err is %v", err)))
+		return err
+	}
+	var db model.Db
+	db.Database = global.GenConfig.System.Name
+	db.Package = strings.ToLower(db.Database)
+	db.Service = utils2.LeftUpper(db.Database)
+
+	db.Option = global.GenConfig
+	db.DriverName = global.GenDB.Name()
+	dir, _ := os.Getwd()
+	pkg, err := golang.GetParentPackage(dir)
+	if err != nil {
+		log.Println(utils2.Red(fmt.Sprintf("GetParentPackage err is %v", err)))
+		return err
+	}
+	db.ParentPkg = pkg + "/" + global.GenConfig.AutoCode.Pkg
+	db.PKG = pkg
+
+	for _, v := range tables {
+		if !strings.HasPrefix(v.Table, global.GenConfig.DB.TablePrefix) {
+			continue
+		}
+		columnData, err := genApp.DB.GetColumn(global.GenConfig.DB.Dbname, v.Table)
+		if err != nil {
+			log.Println(utils2.Red(fmt.Sprintf("获取字段 err is %v", err)))
+			continue
+		}
+		tb, err := columnData.Convert(v.TableComment)
+		if err != nil {
+			fmt.Println(utils2.Red(fmt.Sprintf("数据生成错误错误: %v", err)))
+			continue
+		}
+		if tb.HasTimer {
+			db.HasTimer = true
+		}
+		tb.ParentPkg = db.ParentPkg
+		tb.PKG = db.PKG
+		err = genApp.CreateModel(tb)
+		if err != nil {
+			continue
+		}
+
+		db.Tables = append(db.Tables, tb)
+		db.Email = tb.Email
+		db.Author = tb.Author
+		err = genApp.CreateApiDesc(tb)
+		if err != nil {
+			log.Printf("CreateApiDesc err is %v\n", err)
+		}
+	}
+
+	err = genApp.CreateRpc(&db)
+	if err != nil {
+		log.Printf("CreateRpc err is %v\n", err)
+		return err
+	}
+	err = genApp.CreateApi(&db)
+	if err != nil {
+		log.Printf("CreateApi err is %v\n", err)
+		return err
+	}
+	err = genApp.CreateRpcLogic(&db)
+	if err != nil {
+		log.Printf("CreateRpcLogic err is %v\n", err)
+		return err
+	}
+	//err = genApp.CreateCommon(&db)
+	//if err != nil {
+	//	log.Printf("CreateCommon err is %v\n", err)
+	//	return err
+	//}
+	fmt.Println(utils2.Green("Done!"))
+	return err
+}
--- global/const.go
+++ global/const.go
@@ -7,7 +7,7 @@ import (
 )
 
 const (
-	BuildVersion = "v1.9.88"
+	BuildVersion = "v1.9.89"
 	ProjectName  = "gocode"
 	OsWindows    = "windows"
 	OsMac        = "darwin"
