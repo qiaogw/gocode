b7f8b99a22eb594e5deb861c83bad146e72b2338
qiaogw <qiaogw@sub-cloud.com>
2023-12-24 14:57:23 +0800

    版本

--- cmd/upgrade/upgrade.go
+++ cmd/upgrade/upgrade.go
@@ -2,6 +2,7 @@ package upgrade
 
 import (
 	"fmt"
+	"github.com/qiaogw/gocode/global"
 	"github.com/qiaogw/gocode/util"
 	"runtime"
 
@@ -15,19 +16,31 @@ var Cmd = &cobra.Command{
 	Short: "升级至gocode最新版本",
 	RunE:  upgrade,
 }
+var versionID string
+
+func init() {
+	ver := "latest"
+	Cmd.PersistentFlags().StringVarP(&versionID, "version", "v", ver, "版本号")
+}
 
 // Upgrade gets the latest gocode by
 // go install github.com/qiaogw/gocode@latest
 func upgrade(_ *cobra.Command, _ []string) error {
-	cmd := `GO111MODULE=on GOPROXY=https://goproxy.cn/,direct go install github.com/qiaogw/gocode@latest`
+	currentVersion := fmt.Sprintf(
+		"%s %s/%s", global.BuildVersion,
+		runtime.GOOS, runtime.GOARCH)
+	appCmd := fmt.Sprintf("GOPROXY=https://goproxy.cn/,direct go install github.com/qiaogw/gocode@%s", versionID)
+	cmd := `GO111MODULE=on ` + appCmd
 	if runtime.GOOS == "windows" {
-		cmd = `set GOPROXY=https://goproxy.io,direct && go install github.com/qiaogw/gocode@latest`
+		cmd = `set ` + appCmd
 	}
+	fmt.Println(util.Green(fmt.Sprintf("版本升级开始, 当前版本%s", currentVersion)))
 	_, err := execx.Run(cmd, "")
 	if err != nil {
+		fmt.Println(util.Red(fmt.Sprintf("版本升级失败：%v", err)))
 		return err
 	}
-	//fmt.Println(info, "")
-	fmt.Println(util.Green("Done!"))
+
+	fmt.Println(util.Green(fmt.Sprintf("已升级到最新版本%s", versionID)))
 	return nil
 }
